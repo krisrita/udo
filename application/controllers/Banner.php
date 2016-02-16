c7242b86ec66b6f0fa8b9623c7207061fa226381
guolong <guolong@howdo.cc>
2016-02-16 15:59:19 +0800

    add more banners

--- application/controllers/Banner.php
+++ application/controllers/Banner.php
@@ -17,4 +17,36 @@ class BannerController extends Base_Contr
     function formAction(){
 
     }
+    function getImageHtmlAction(){
+        $imageUrl = $this->get('image');
+        $htmlStr =
+<<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title></title>
+    <style type="text/css">
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .bg {
+        position: relative;
+    }
+    .bg-img {
+        width: 100%;
+    }
+    </style>
+    <body>
+    	<div class="bg">
+    		<img src="{$imageUrl}" class="bg-img"/>
+    	</div>
+
+    </body>
+</html>
+HTML;
+        echo $htmlStr;exit;
+    }
 }
